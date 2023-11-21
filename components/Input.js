bdb80011eff5243d36a2f4fdeca1495f556686c5
mmnldm <mremmanueledem@gmail.com>
2023-11-21 01:01:23 +0100

    feat: add error message

--- components/Input.js
+++ components/Input.js
@@ -12,11 +12,11 @@ export default function Input(props) {
   const [secondActor, setSecondActor] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const removeClass = (first) => {
-    document.getElementById(first).classList.remove("hidden");
+  const removeClass = (id) => {
+    document.getElementById(id).classList.remove("hidden");
 
     setTimeout(() => {
-      document.getElementById(first).classList.add("hidden");
+      document.getElementById(id).classList.add("hidden");
     }, 2000);
     setIsLoading(false);
   };
@@ -49,8 +49,13 @@ export default function Input(props) {
         }
         props.parentFunction(filteredMovies);
         setIsLoading(false);
-
-        console.log(filteredMovies);
+      } else if (firstId && !secondId) {
+        removeClass("second_error_message");
+      } else if (secondId && !firstId) {
+        removeClass("first_error_message");
+      } else {
+        removeClass("first_error_message");
+        removeClass("second_error_message");
       }
     }
   };
@@ -61,18 +66,26 @@ export default function Input(props) {
         <label className=" lg:space-x-14 space-x-4 justify-center items-center font-space-grotesk">
           <input
             type="text"
+            id="first_input"
             onChange={handleFirstInput}
             placeholder="E.g Anne Hattaway"
             className="bg-star-gray lg:pr-4 lg:pl-6 lg:py-3 w-[145px] h-15 px-3 py-1 lg:w-auto lg:h-auto rounded-md text-black lg:text-xl placeholder:text-gray-400 placeholder:font-space-grotesk placeholder:text-[13px] lg:placeholder:text-xl focus:outline-none focus:ring-[#6d3daf] focus:ring-2"
           />
+          <p className="hidden text-red-600" id={"first_error_message"}>
+            Something's wrong here
+          </p>
         </label>
         <label className="lg:space-x-14 space-x-4 justify-center items-center font-space-grotesk">
           <input
             type="text"
+            id="second_input"
             onChange={handleSecondInput}
             placeholder="E.g Meryl Streep"
             className="bg-star-gray lg:pr-4 lg:pl-6 lg:py-3 w-[145px] h-15 px-3 py-1 lg:w-auto lg:h-auto rounded-md text-black lg:text-xl placeholder:text-gray-400 placeholder:font-space-grotesk placeholder:text-[13px] lg:placeholder:text-xl focus:outline-none focus:ring-[#6d3daf] focus:ring-2"
           />
+          <p className="hidden text-red-600" id={"second_error_message"}>
+            Something's wrong here
+          </p>
         </label>
       </form>
       <button
