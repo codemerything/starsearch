53f13f5d0121cef4a2f82e9d5d629601ec07bb2a
mmnldm <mremmanueledem@gmail.com>
2023-11-21 01:05:17 +0100

    fix: remove ' mark from input component

--- components/Input.js
+++ components/Input.js
@@ -72,7 +72,7 @@ export default function Input(props) {
             className="bg-star-gray lg:pr-4 lg:pl-6 lg:py-3 w-[145px] h-15 px-3 py-1 lg:w-auto lg:h-auto rounded-md text-black lg:text-xl placeholder:text-gray-400 placeholder:font-space-grotesk placeholder:text-[13px] lg:placeholder:text-xl focus:outline-none focus:ring-[#6d3daf] focus:ring-2"
           />
           <p className="hidden text-red-600" id={"first_error_message"}>
-            Something's wrong here
+            Somethings wrong here
           </p>
         </label>
         <label className="lg:space-x-14 space-x-4 justify-center items-center font-space-grotesk">
@@ -84,7 +84,7 @@ export default function Input(props) {
             className="bg-star-gray lg:pr-4 lg:pl-6 lg:py-3 w-[145px] h-15 px-3 py-1 lg:w-auto lg:h-auto rounded-md text-black lg:text-xl placeholder:text-gray-400 placeholder:font-space-grotesk placeholder:text-[13px] lg:placeholder:text-xl focus:outline-none focus:ring-[#6d3daf] focus:ring-2"
           />
           <p className="hidden text-red-600" id={"second_error_message"}>
-            Something's wrong here
+            Somethings wrong here
           </p>
         </label>
       </form>
