42d8c9646e88caa9b84c4134391fc497f95662c7
mmnldm <mremmanueledem@gmail.com>
2024-02-13 17:39:42 +0100

    feat: add color to text

--- components/Error.js
+++ components/Error.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Error({ cname, message }) {
-  return <div className={cname}>{message}</div>;
+  return <div className="bg-red-500">{message}</div>;
 }
